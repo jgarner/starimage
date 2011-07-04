70e61a5ac23d4e088c25b84a69dc3ad53dc52d7c
Joshua Garner <joshuajamesgarner@gmail.com>
2011-07-04 20:32:21 +1200

    get_doc now takes a base_url optional param that will make relative img srcs absolute which means we can find content length later. If get_doc first param is a url and base_url is set will use base_url but if no base_url set will get the base_url from the url and use it. Added get_doc_from_url tests and functionality and generally improved tests.

--- starimage.py
+++ starimage.py
@@ -2,9 +2,6 @@ import logging
 import urlparse
 import re
 import lxml.html
-
-def extract(url_or_html):
-    return True
             
 def is_url(url):
     if url == None:
@@ -18,37 +15,51 @@ def is_html(html):
         return False
     else:
         return re.search('<html.*?>', html, re.I|re.S) != None   
-    
-def get_doc(url_or_html):
+  
+def handle_exception(message):
+     logging.error('starimage: ' + message)
+             
+def get_doc_from_url(url):
+    doc = None
+    try:
+       doc = lxml.html.parse(url).getroot()   
+    except IOError, e:
+        handle_exception('Error opening url: ' + url)
+    return None
+              
+def get_doc(url_or_html, base_url=None):
+    doc = None
+    from_url = False
     try:
         if url_or_html == None:
-            return None
+            doc = None
         elif is_url(url_or_html):
-            doc = lxml.html.parse(url_or_html).getroot()
-            parts = urlparse.urlparse(url_or_html)
-            if parts.scheme in ['http', 'https'] and parts.hostname != None:
-                doc.make_links_absolute(parts.scheme + '://' + parts.hostname) 
-            return doc
+            doc = get_doc_from_url(url_or_html)
+            from_url = True
         elif is_html(url_or_html):
-            return lxml.html.document_fromstring(url_or_html)
+            doc = lxml.html.document_fromstring(url_or_html)
         else:
-            return lxml.html.fragment_fromstring(url_or_html)
-    except IOError, e:
-        handle_exception('Error opening url: ' + url_or_html)
+            doc = lxml.html.fragment_fromstring(url_or_html)
     except lxml.etree.ParserError, e:
         handle_exception('Error parsing HTML')
+    else:    
+        if doc != None:
+            if base_url == None and from_url == True:            
+                parts = urlparse.urlparse(url_or_html)
+                if parts.scheme in ['http', 'https'] and parts.hostname != None:
+                    base_url = parts.scheme + '://' + parts.hostname
+            if base_url != None:
+                doc.make_links_absolute(base_url)        
+    return doc
                     
 def get_images(doc):
-    if doc == None:
-        return None
-    else:
-        return doc.xpath('//img')
+    return True
     
 def get_largest_image():
     return True
     
 def get_image_content_length():
-    return True
-
-def handle_exception(message):
-     logging.error('starimage: ' + message)   
+    return True   
+     
+def extract(url_or_html):
+    return True     
--- starimage_test.py
+++ starimage_test.py
@@ -7,74 +7,78 @@ import unittest
 class TestStarImage(unittest.TestCase):
     
     def test_interface(self):
-        self.assertIsNotNone(starimage.extract)        
+        self.assertIsNotNone(starimage.handle_exception)     
         self.assertIsNotNone(starimage.is_url)
         self.assertIsNotNone(starimage.is_html)
+        self.assertIsNotNone(starimage.get_doc_from_url)
         self.assertIsNotNone(starimage.get_doc)
         self.assertIsNotNone(starimage.get_images)
         self.assertIsNotNone(starimage.get_largest_image)
-        self.assertIsNotNone(starimage.get_image_content_length)
-        self.assertIsNotNone(starimage.handle_exception)
+        self.assertIsNotNone(starimage.get_image_content_length)        
+        self.assertIsNotNone(starimage.extract)  
        
-    # test is_url    
+# test is_url    
     def test_is_url_return_false_if_url_equals_none(self):
         self.assertFalse(starimage.is_url(None))
-        
+
     def test_is_url_return_false_if_not_http_or_https(self):
         self.assertFalse(starimage.is_url('ftp://example'))
-    
+
     def test_is_url_return_true_if_http(self):
         self.assertTrue(starimage.is_url('http://example.com')) 
-        
+
     def test_is_url_return_true_if_https(self):
         self.assertTrue(starimage.is_url('https://example.com'))           
-        
-    # test is_html    
+
+# test is_html    
     def test_is_html_return_false_if_equals_none(self):
         self.assertFalse(starimage.is_html(None))  
-        
+
     def test_is_html_return_false_if_no_html_tag_found(self):
         self.assertFalse(starimage.is_html("sfsf<img>"))
-        
+
     def test_is_html_return_true_if_html_tag_found(self):
         self.assertTrue(starimage.is_html('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html>'))        
-        
+
     def test_is_html_return_true_if_html_tag_found_with_attributes(self):
         self.assertTrue(starimage.is_html('<HTML xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'))        
-        
-    # test get_doc
+
+# test get_doc_from_url
+    @patch('lxml.html.parse')
+    def test_get_doc_from_url_raises_ioerror_with_invalid_url(self, parse_mock):
+        parse_mock.side_effect = IOError()
+        self.assertRaises(IOError, starimage.get_doc_from_url('http://invalidurl.com'))    
+
+# test get_doc
     def test_get_doc_return_none_if_param_is_none(self):
         self.assertIsNone(starimage.get_doc(None))
 
-    @patch('lxml.html.parse')
-    def test_get_doc_raises_ioerror_with_invalid_url(self, parse_mock):
-        parse_mock.side_effect = IOError()
-        self.assertRaises(IOError, starimage.get_doc('http://1234abcdef.co.nz.au'))      
-        
+    @patch('starimage.get_doc_from_url')
+    def test_get_doc_returns_none_with_invalid_url(self, get_doc_from_url_mock):
+        get_doc_from_url_mock.return_value = None
+        self.assertIsNone(starimage.get_doc('http://invalidurl.com'))
+
+    @patch('starimage.get_doc_from_url')
+    def test_get_doc_returns_element_with_valid_url(self, parse_mock):
+        parse_mock.return_value = lxml.html.document_fromstring('<html></html>')
+        self.assertIsInstance(starimage.get_doc('http://example.com'), lxml.etree._Element)               
+
     def test_get_doc_raises_parsererror_with_empty_string(self):
         self.assertRaises(lxml.etree.ParserError, starimage.get_doc(''))    
-        
+
     def test_get_doc_returns_none_with_empty_string(self):
         self.assertIsNone(starimage.get_doc('')) 
-        
+
     def test_get_doc_returns_instance_of_element_with_valid_html(self):
-        self.assertIsInstance(starimage.get_doc('<html><head></head><body><div>Hi</div></body></html>'), lxml.etree._Element)   
-        
+        self.assertIsInstance(starimage.get_doc('<html><html>'), lxml.etree._Element)   
+
     def test_get_doc_returns_instance_of_element_with_valid_html_fragment(self):
         self.assertIsInstance(starimage.get_doc('<div>Hi</div>'), lxml.etree._Element)  
-    
-    # test get_images   
-    def test_get_images_return_none_if_doc_is_none(self):
-        self.assertIsNone(starimage.get_images(None))    
-        
-    def test_get_images_return_empty_list_if_doc_has_no_images(self):
-        doc = starimage.get_doc('<html></html>')
-        self.assertTrue(len(starimage.get_images(doc)) == 0) 
-                    
-    def test_get_images_return_list_of_images_if_doc_has_images_from_html(self):
-        doc = starimage.get_doc('<html><header></header><body><div><img src="/test.jpg" /><img src="/logo.gif" /></body></html>')
-        imgs = starimage.get_images(doc)
-        self.assertTrue(imgs[0].tag == 'img')                                 
+
+    def test_get_doc_makes_imgs_absolute_with_base_url(self):
+        doc = starimage.get_doc('<html><body><img src="/img1.gif" /></body></html>', 'http://example.com')
+        imgs = doc.xpath('//img')
+        self.assertEquals(imgs[0].get('src'), 'http://example.com/img1.gif')                                
         
 if __name__ == '__main__':
     unittest.main()
